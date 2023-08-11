9b596b8df2d98f10720a1659cea8ce236c8f2013
abrahan <abrahandev@gmail.com>
2023-08-11 15:26:58 +0200

    Utilizamos el createElement para el div de la ramdomMeal

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 
+const meals = document.getElementById('meals')
+
 async function getRandomMeal() {
-    const randomMeal = fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+
+    const respData = await resp.json()
+    const randomMeal = respData.meals[0]
+
+    addMeal(randomMeal, true);
+    console.log(randomMeal)
 }
 
 async function getMealById(id) {
-    const meal = fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+id)
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+id)
 }
 
 async function getMealsBySearch(term) {
-    const meals = fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+term)
+    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+term)
+}
+
+getRandomMeal()
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div')
+    meal.classList.add('meal') 
+
+    meal.innerHTML = `
+    <div class="meal-header">
+            ${random ? `
+                <span class="random">
+                Ramdom Recipe
+                </span>
+            ` : '' }
+              <img src="${mealData.strMealThumb} "
+                 alt="${mealData.strMeal} "
+            />
+        </div>
+        <div class="meal-body">
+            <h4> ${mealData.strMeal} </h4>
+            <button class="fav-btn active">
+                <i class="fas fa-heart"></i>
+              
+            </button>
+        </div>
+    `;
+
+    meals.appendChild(meal)
 }
